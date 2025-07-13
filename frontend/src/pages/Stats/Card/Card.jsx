c3127bf2ee4fed7d719f8fd6dcd2990e91ac5a1a
HappyHiTech <harveycytan@gmail.com>
2025-07-13 15:08:38 -0700

    revert back

--- frontend/src/pages/Stats/Card/Card.jsx
+++ frontend/src/pages/Stats/Card/Card.jsx
@@ -2,8 +2,8 @@ import "./Card.css"
 import {useState, useRef, useEffect} from 'react';
 import { useUpload } from "../../../hook/ClickContent";
 
-export default function Card() {
-    const [totalTime, setTotalTime] = useState('')
+export default function Card({title, clickFunc}) {
+    const [listenTime, setListenTime] = useState('')
     const {listenTimeData, uploaded} = useUpload();
     const monthButtonRef = useRef(null);
 
@@ -21,23 +21,23 @@ export default function Card() {
         const currentYear = listenTimeData["currentYear"];
 
         if (type == "AT"){
-            setTotalTime(allTime);
+            setListenTime(allTime);
         }
         else if (type == "CM"){
-            setTotalTime(currentMonth);
+            setListenTime(currentMonth);
         }
         else if (type == "CY"){
-            setTotalTime(currentYear);
+            setListenTime(currentYear);
         }
     }
 
     return (
         <div className="card">
-            <header className="card-header"> Total Amount Listened (mins)</header>
+            <header className="card-header">{title}</header>
             <button onClick={() => handleClick("CM")} className="card-button" ref={monthButtonRef}>Current Month</button>
             <button onClick={() => handleClick("CY")} className="card-button">Current Year</button>
             <button onClick={() => handleClick("AT")} className="card-button">All Time</button>
-            <div className="card-main">{totalTime} Minutes</div>
+            <div className="card-main">{listenTime} Minutes</div>
         </div>
     );
 }
--- frontend/src/pages/Stats/Stats.jsx
+++ frontend/src/pages/Stats/Stats.jsx
@@ -1,10 +1,14 @@
 import "./Stats.css"
 import Card from "./Card/card";
+import { useState } from "react";
+import { useUpload } from "../../hook/ClickContent";
 
 export default function Stats(){
+
     return (
         <div className="stats-body">    
             <Card />
+            
         </div>
     );
 }
